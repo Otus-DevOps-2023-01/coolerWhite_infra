9bae31b21e5c2fe0b6b96d278cd7b353a2ce2d8d
coolerWhite <ronaldinio09@yandex.ru>
2023-03-21 22:37:20 +0300

    test4

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -15,14 +15,14 @@ provider "yandex" {
   zone                     = var.zone
 }
 module "app" {
-  source          = "/home/otus/coolerWhite_infra/terraform/modules/app"
+  source          = "../modules/app"
   public_key_path = var.public_key_path
   app_disk_image  = var.app_disk_image
   subnet_id       = var.subnet_id
 }
 
 module "db" {
-  source          = "/home/otus/coolerWhite_infra/terraform/modules/db"
+  source          = "../modules/app"
   public_key_path = var.public_key_path
   db_disk_image   = var.db_disk_image
   subnet_id       = var.subnet_id
