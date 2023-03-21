6ad3ef76863f1ab3a5a553a667bbd05f711b2f99
coolerWhite <ronaldinio09@yandex.ru>
2023-03-21 22:05:07 +0300

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-#terraform {
-#  required_providers {
-#    yandex = {
-#      source = "yandex-cloud/yandex"
-#    }
-#  }
-#  required_version = ">= 0.13"
-#}
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
 
 // Configure the Yandex.Cloud provider
 provider "yandex" {
@@ -14,51 +14,16 @@ provider "yandex" {
   folder_id                = var.folder_id
   zone                     = var.zone
 }
+module "app" {
+  source          = "/home/otus/coolerWhite_infra/terraform/modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+  subnet_id       = var.subnet_id
+}
 
-// Create a new instance
-
-resource "yandex_compute_instance" "app" {
-  name = "reddit-app"
-
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      # Указать id образа созданного в предыдущем домашем задании
-      image_id = var.image_id
-    }
-  }
-
-  network_interface {
-    # Указан id подсети default-ru-central1-a
-    subnet_id = var.subnet_id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file(var.public_key_path)}"
-  }
-
-  connection {
-    type  = "ssh"
-    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
-    user  = "ubuntu"
-    agent = false
-    # путь до приватного ключа
-    private_key = file(var.private_key_path)
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
-
-
+module "db" {
+  source          = "/home/otus/coolerWhite_infra/terraform/modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+  subnet_id       = var.subnet_id
 }
