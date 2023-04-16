2120144e9b7f4efd0a6cd7f51e6be34181bcbc93
coolerWhite <ronaldinio09@yandex.ru>
2023-04-16 14:16:03 +0300

    ansible-4

--- git_push.sh
+++ git_push.sh
@@ -3,5 +3,5 @@ cd ~/coolerwhite_infra
 pre-commit run --all-files
 pre-commit run --all-files
 git add .
-git commit -m " ansible-3 "
-git push --set-upstream origin ansible-3
+git commit -m " ansible-4 "
+git push --set-upstream origin ansible-4
